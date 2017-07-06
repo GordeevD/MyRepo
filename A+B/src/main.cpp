497f84e1c3bc4b9fba432ef17058760d17872565
DG <me@dmitrygordeev.ru>
2017-07-06 21:31:42 +0300

    First try

---
+++ A+B/src/main.cpp
@@ -0,0 +1,16 @@
+//============================================================================
+// Name        : A+B.cpp
+// Author      : DMITRY GORDEEV
+// Version     :
+// Copyright   : Your copyright notice
+// Description : A+B in C++, Ansi-style
+//============================================================================
+#include <iostream>
+using namespace std;
+
+int main() {
+  int a, b;
+  cin >> a >> b;
+  cout << a + b << endl;
+  return 0;
+}

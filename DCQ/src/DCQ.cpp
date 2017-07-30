02abf687b90ed77e3363211a58a5db4a06fd552c
dinn <dinn@192.168.1.233>
2017-07-30 23:07:40 +0300

    This is the messanger what you can use to say hi to your friends and be happy to get greetings back.

---
+++ DCQ/src/DCQ.cpp
@@ -0,0 +1,15 @@
+//============================================================================
+// Name        : DCQ.cpp
+// Author      : DMITRY GORDEEV
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "DCQ message" << endl; // prints DCQ message
+	return 0;
+}
